359ccd6419e0025bbe309caadd871bc6abf7de44
anik-ghosh-au7 <tech.anikghosh@gmail.com>
2023-09-21 15:18:56 +0530

    updates

--- main.go
+++ main.go
@@ -324,7 +324,10 @@ func diff(ipfs icore.CoreAPI, file string, ctx context.Context) error {
 }
 
 // Clone a repo by its CID
-func clone(ipfs icore.CoreAPI, cid string, ctx context.Context) error {
+func clone(ipfs icore.CoreAPI, cid string, peers []string, ctx context.Context) error {
+	if err := connectToPeers(ctx, ipfs, peers); err != nil {
+		return fmt.Errorf("failed to connect to peers: %s", err)
+	}
 	// For demonstration, let's assume cloning involves getting content by CID.
 	contentNode, err := ipfs.Unixfs().Get(ctx, icorepath.New(cid))
 	if err != nil {
@@ -381,7 +384,24 @@ func executeCommand(cmd string, options []string, ipfs icore.CoreAPI, ctx contex
 		if len(options) < 1 {
 			return fmt.Errorf("please specify a CID to clone")
 		}
-		return clone(ipfs, options[0], ctx)
+		examplePeers := []string{
+			// IPFS Bootstrapper nodes
+			"/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
+			"/dnsaddr/bootstrap.libp2p.io/p2p/QmQCU2EcMqAqQPR2i9bChDtGNJchTbq5TbXJJ16u19uLTa",
+			"/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb",
+			"/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt",
+			// IPFS Cluster Pinning nodes
+			"/ip4/138.201.67.219/tcp/4001/p2p/QmUd6zHcbkbcs7SMxwLs48qZVX3vpcM8errYS7xEczwRMA",
+			"/ip4/138.201.67.219/udp/4001/quic/p2p/QmUd6zHcbkbcs7SMxwLs48qZVX3vpcM8errYS7xEczwRMA",
+			"/ip4/138.201.67.220/tcp/4001/p2p/QmNSYxZAiJHeLdkBg38roksAR9So7Y5eojks1yjEcUtZ7i",
+			"/ip4/138.201.67.220/udp/4001/quic/p2p/QmNSYxZAiJHeLdkBg38roksAR9So7Y5eojks1yjEcUtZ7i",
+			"/ip4/138.201.68.74/tcp/4001/p2p/QmdnXwLrC8p1ueiq2Qya8joNvk3TVVDAut7PrikmZwubtR",
+			"/ip4/138.201.68.74/udp/4001/quic/p2p/QmdnXwLrC8p1ueiq2Qya8joNvk3TVVDAut7PrikmZwubtR",
+			"/ip4/94.130.135.167/tcp/4001/p2p/QmUEMvxS2e7iDrereVYc5SWPauXPyNwxcy9BXZrC1QTcHE",
+			"/ip4/94.130.135.167/udp/4001/quic/p2p/QmUEMvxS2e7iDrereVYc5SWPauXPyNwxcy9BXZrC1QTcHE",
+		}
+
+		return clone(ipfs, options[0], examplePeers, ctx)
 	default:
 		return fmt.Errorf("unknown command: %s", cmd)
 	}
